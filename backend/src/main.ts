afb9890ccff12077cb5eb933966c5604f5ce1063
mr-Deviant <stanislav@nika.org.ua>
2023-12-03 20:20:23 +0200

    Pass app port from env variables

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService: ConfigService = app.get<ConfigService>(ConfigService);
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(configService.get('APP_PORT') || 80);
 }
 bootstrap();
